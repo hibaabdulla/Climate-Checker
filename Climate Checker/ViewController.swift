55c13705490fdda0bdcfcb194c630181aab2f0d3
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-05-25 15:00:44 -0700

    Initial Commit

---
+++ Climate Checker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Climate Checker
+//
+//  Created by Hiba Abdulla on 5/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
