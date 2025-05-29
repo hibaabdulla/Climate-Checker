59340dd2328e158510ea68ecbc36b34f4fa3c6be
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-05-28 22:30:52 -0700

    Decode JSON

---
+++ Climate Checker/Model/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  Climate Checker
+//
+//  Created by Hiba Abdulla on 5/28/25.
+//
+
+import Foundation
---
+++ Climate Checker/View/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Climate Checker
+//
+//  Created by Hiba Abdulla on 5/26/25.
+//
+
+import Foundation
